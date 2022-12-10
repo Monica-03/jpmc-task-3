0e378803b2dc50c8597d4b74f604473ae1b05497
Monica-03 <100765138+Monica-03@users.noreply.github.com>
2022-12-10 20:52:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,65 @@
 import React, { Component } from 'react';
-import DataStreamer, { ServerRespond } from './DataStreamer';
-import Graph from './Graph';
-import './App.css';
+import { Table, TableData } from '@finos/perspective';
+import { ServerRespond } from './DataStreamer';
+import { DataManipulator } from './DataManipulator';
+import './Graph.css';
 
-interface IState {
+interface IProps {
   data: ServerRespond[],
-  showGraph: boolean,
 }
 
-class App extends Component<{}, IState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      data: [],
-      showGraph: false,
-    };
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void,
+}
+class Graph extends Component<IProps, {}> {
+  table: Table | undefined;
+
+  render() {
+    return React.createElement('perspective-viewer');
   }
 
-  renderGraph() {
-    if (this.state.showGraph) {
-      return (<Graph data={this.state.data}/>)
+  componentDidMount() {
+    // Get element from the DOM.
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+
+    const schema = {
+      price_abc:'float',
+      price_def:'float',
+      ratio:'float',
+      timestamp: 'date',
+      upper_bound:'float',
+      lower_bound:'float',
+      trigger_alert:'float',
+    };
+
+    if (window.perspective && window.perspective.worker()) {
+      this.table = window.perspective.worker().table(schema);
+    }
+    if (this.table) {
+      // Load the `table` in the `<perspective-viewer>` DOM reference.
+      elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+      elem.setAttribute('aggregates', JSON.stringify({
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg', 
+      }));
     }
   }
 
-  getDataFromServer() {
-    let x = 0;
-    const interval = setInterval(() => {
-      DataStreamer.getData((serverResponds: ServerRespond[]) => {
-        this.setState({
-          data: serverResponds,
-          showGraph: true,
-        });
-      });
-      x++;
-      if (x > 1000) {
-        clearInterval(interval);
-      }
-    }, 100);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          Bank Merge & Co Task 3
-        </header>
-        <div className="App-content">
-          <button className="btn btn-primary Stream-button" onClick={() => {this.getDataFromServer()}}>Start Streaming Data</button>
-          <div className="Graph">
-            {this.renderGraph()}
-          </div>
-        </div>
-      </div>
-    )
+  componentDidUpdate() {
+    if (this.table) {
+      this.table.update([
+        DataManipulator.generateRow(this.props.data),
+      ] as unknown as TableData);
+    }
   }
 }
 
-export default App;
+export default Graph;
